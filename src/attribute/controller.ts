bfc8f23f8cd9196fe8a67a7d295ced6bede3c182
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2024-06-07 14:02:35 +0900

    remove unused import

--- src/attribute/controller.ts
+++ src/attribute/controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get } from "@nestjs/common";
 import * as newrelic from "newrelic";
 
 import { AttributeService } from "./service";
